a5a6efea4763fb21d77bd26a88dc2c9508ad8750
Juan Morales <jmoralespuello@gmail.com>
2024-10-04 10:03:31 -0500

    ADD: Script SQL helper

---
+++ database/first_script.sql
@@ -0,0 +1,29 @@
+show databases;
+
+create database contacts_laravel_app;
+
+use contacts_laravel_app;
+
+show tables;
+
+create table contact(
+	id int primary key,
+    name varchar(45),
+    last_name varchar(45),
+    age smallint,
+    gender char(1),
+    email varchar(60),
+    phone_number varchar(25),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+describe contact;
+
+select * from contact;
+
+drop table contact;
+
+truncate table migrations;
+
+select * from migrations;
