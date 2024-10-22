1d2083328d15d7b056e0e0f6cd191b59a38df724
Juan Morales <jmoralespuello@gmail.com>
2024-10-22 09:16:26 -0500

    ADD: ContactPhoto migration file

---
+++ database/migrations/2024_10_22_134332_create_photo_contact_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photo_contact', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename', 100)->default('contactphoto');
+            $table->integer('contact_id')->default(0);
+            $table->datetime('uploaded_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photo_contact');
+    }
+};
