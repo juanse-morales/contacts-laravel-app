7ebab870b36a9cbafb9c295eae4eb673673a5b70
Juan Morales <jmoralespuello@gmail.com>
2024-10-22 09:51:41 -0500

    REFACTOR: Add upload_photo function on ContactPhotoController

--- app/Http/Controllers/ContactPhotoController.php
+++ app/Http/Controllers/ContactPhotoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\ContactPhoto;
+
 class ContactPhotoController extends Controller
 {
     /**
@@ -44,5 +46,77 @@ class ContactPhotoController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    /**
+     * Private function to obtain current date formatted
+     */
+    private function getDateFormatted(){
+      // Get current date
+      $fechaCarbon = Carbon::now();
+
+      // Get microsegundos and convert to milisegundos
+      $microsegundos = $fechaCarbon->format('u'); // Esto da los microsegundos (ej. 123456)
+      $milisegundos = substr($microsegundos, 0, 5); // Tomamos solo los primeros 5 dígitos
+      
+      // Format date 
+      $date = $fechaCarbon->format('Y-m-d H-i-s') . '-' . $milisegundos;
+      return $date;
+  }
+
+    /**
+     * Endpoint for uploading contact photo file
+     */
+    public function upload_photo(string $id) {
+      if (count(request()->photo) > 0) {
+        // If any of files exist then we save them
+        $archivos = array();
+        foreach (request()->photo as $files) {
+          $original_name  = $files->getClientOriginalName();
+          
+          // Split filename of its extension
+          $filename = pathinfo($original_name, PATHINFO_FILENAME);
+          $file_extension = $files->getClientOriginalExtension();
+
+          // Create new file with identifier
+          $date = $this->getDateFormatted();
+          //$new_file = strtoupper($filename). ' '. $date. '.'. strtolower($file_extension);
+          $new_filename = 'CONTACTPHOTO' . '_' . $id . '_' . $date . '.' . strtolower($file_extension);
+
+          $tempFile = $files;
+          $filepath = storage_path('files/');
+          $file = $filepath . $new_filename;
+          $fecha_creacion = now();
+          $archivos[] = $file;
+          
+          $values = array(
+            'contact_id' => request()->id_municipio_libreta,
+            'original_filename' => $original_name,
+            'new_filename' => $new_filename,
+            'uploaded_data' => $fecha_creacion,
+            'ID_USUARIO' => request()->id_usuario
+          );
+          
+          $contact_photo = ContactPhoto::create($values);
+
+          
+          move_uploaded_file($tempFile, $file);
+        }
+
+        $data = [
+          "message" => "Uploaded files to success",
+          "status" => 201,
+          "files" => $archivos
+        ];
+        
+        return response()->json($data, 201);
+      } else {
+        $data = [
+          'message'=> 'No files received',
+          'status' => 400
+        ];
+        
+        return response()->json($data, 400);
+      }
     }
 }
