a7a441431aabc2beea1229ec153e76e5178e7152
Juan Morales <jmoralespuello@gmail.com>
2024-10-22 09:50:36 -0500

    REFACTOR: reorganize api routes

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactPhotoController;
 
-Route::get('/contact/index/{filter}', [ContactController::class, 'index']);
+Route::group(['prefix' => 'contact'], function() {
+  Route::get('index/{filter}', [ContactController::class, 'index']);
+  Route::get('archived/{filter}', [ContactController::class, 'getArchived']);
+  Route::get('blocked/{filter}', [ContactController::class, 'getBlocked']);
+  Route::get('deleted/{filter}', [ContactController::class, 'getDeleted']);
+  Route::get('{id}', [ContactController::class, 'show']);
+  Route::post('', [ContactController::class, 'store']);
+  Route::put('/{id}', [ContactController::class, 'update']);
+  Route::delete('/{id}', [ContactController::class, 'destroy']);
+  Route::put('/restore/{id}', [ContactController::class, 'restore']);
+  Route::put('/block/{id}', [ContactController::class, 'block']);
+  Route::put('/archive/{id}', [ContactController::class, 'archive']);
+});
 
-Route::get('/contact/archived/{filter}', [ContactController::class, 'getArchived']);
-
-Route::get('/contact/blocked/{filter}', [ContactController::class, 'getBlocked']);
-
-Route::get('/contact/deleted/{filter}', [ContactController::class, 'getDeleted']);
-
-Route::get('/contact/{id}', [ContactController::class, 'show']);
-
-Route::post('/contact', [ContactController::class, 'store']);
-
-Route::put('/contact/{id}', [ContactController::class, 'update']);
-
-Route::delete('/contact/{id}', [ContactController::class, 'destroy']);
-
-Route::put('/contact/restore/{id}', [ContactController::class, 'restore']);
-
-Route::put('/contact/block/{id}', [ContactController::class, 'block']);
-
-Route::put('/contact/archive/{id}', [ContactController::class, 'archive']);
+Route::group(['prefix' => 'photo'], function() {
+  Route::post('upload/{id}', [ContactPhotoController::class, 'upload_photo']);
+});
